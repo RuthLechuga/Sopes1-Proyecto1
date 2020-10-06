f25347111c8baaa797057704bd5dc7b3d6c2e72c
Ruth Lechuga <ruthlechuga.1997@gmail.com>
2020-10-05 22:12:20 -0600

    implementacion de simulacion de balanceador de carga servidor 1

---
+++ Servidor 1/app/run.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from bson.json_util import dumps, loads
+import requests
+
+app = Flask(__name__)
+pServidorA = 'http://35.224.184.225:5000/'
+pServidorB = 'http://35.184.254.66:5000/'
+
+@app.route('/postear',methods=['POST'])
+def index():
+    if request.method == 'POST':
+        content = request.json
+        autor = content['autor']
+        nota = content['nota']
+        datos = {'autor':autor, 'nota': nota}
+        datosServidorA = loads(requests.get(pServidorA+ 'datos_servidor').content)
+        datosServidorB = loads(requests.get(pServidorB+ 'datos_servidor').content)
+
+        if datosServidorA['cantidadDatos'] < datosServidorB['cantidadDatos']:
+            return requests.post(pServidorA, json = datos).content
+        
+        if datosServidorA['cantidadDatos'] > datosServidorB['cantidadDatos']:
+            return requests.post(pServidorB, json = datos).content
+        
+        if datosServidorA['RAM'] < datosServidorB['RAM']:
+            return requests.post(pServidorA, json = datos).content
+        
+        if datosServidorA['RAM'] > datosServidorB['RAM']:
+            return requests.post(pServidorB, json = datos).content
+        
+        if datosServidorA['CPU'] < datosServidorB['CPU']:
+            return requests.post(pServidorA, json = datos).content
+        
+        if datosServidorA['CPU'] > datosServidorB['CPU']:
+            return requests.post(pServidorB, json = datos).content
+        
+        return requests.post(pServidorA, json = datos).content
+        
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
