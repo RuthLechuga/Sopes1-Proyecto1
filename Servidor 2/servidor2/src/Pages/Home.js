a385b6fe05819be5f76c1fe7c6f3037add0414d9
Ruth Lechuga <ruthlechuga.1997@gmail.com>
2020-10-10 12:25:03 -0600

    servidor A,b: modulo cpu

--- Servidor 2/servidor2/src/Pages/Home.js
+++ Servidor 2/servidor2/src/Pages/Home.js
@@ -1,18 +1,82 @@
 import React from 'react'
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import NoteIcon from '@material-ui/icons/Note';
+import './Home.css'
 
 class Home extends React.Component {
-    render() {
-      return (
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">Servidores</Dropdown.Toggle>
+
+  servidorA = "http://34.123.224.145"
+  servidorB = "http://35.226.94.227"
+
+  constructor(props){
+    super(props)
+    this.state = { servidor: 1 }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({servidor: e.target.value})
+    console.log(this.state.servidor)
+  }
+
+  generate(element) {
+    return [0, 1, 2].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      }),
+    );
+  }
+  
+
+  render() {
+    return (
+      <div style={{width:100+'%'}}>
+        <FormControl variant="filled" style={{width:100+'%'}}>
+          <InputLabel htmlFor="filled-age-native-simple">Servidor</InputLabel>
+          <Select
+            native
+            value={this.state.servidor}
+            onChange={this.handleChange}
+            style={{width:100+'%'}}
+            inputProps={{
+              name: 'age',
+              id: 'filled-age-native-simple',
+            }}
+          >
+            <option value={1}>Servidor A</option>
+            <option value={2}>Servidor B</option>
+          </Select>
+        </FormControl>
         
-            <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Servidor A</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Servidor B</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-      )
-    }
+        <div style={{paddingLeft: 10+'%', width: 80+'%'}}>
+          <div>
+            <List>
+              {this.generate(
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <NoteIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Single-line item"
+                  />
+                </ListItem>,
+              )}
+            </List>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default Home  
