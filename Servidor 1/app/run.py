34f982a8ba8ad3fc92926d06be9b2c1e3044fe28
Ruth Lechuga <ruthlechuga.1997@gmail.com>
2020-10-10 21:49:57 -0600

    cors server 1

--- Servidor 1/app/run.py
+++ Servidor 1/app/run.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
 from bson.json_util import dumps, loads
 import requests
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['MONGO_DBNAME'] = 'oraciones'
+app.config['MONGO_URI'] = 'mongodb://mongo:27017/oraciones'
+
 pServidorA = 'http://34.123.224.145:5000/'
 pServidorB = 'http://35.226.94.227:5000/'
 
 @app.route('/postear',methods=['POST'])
+@cross_origin()
 def index():
     if request.method == 'POST':
         content = request.json
